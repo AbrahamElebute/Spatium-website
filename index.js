9eb8421d2ec27cfc8ee503a783704ad0b27d3dd3
AbrahamElebute <elebuteimisioluwa@gmail.com>
2022-04-03 20:44:52 +0100

    Spatium-website

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const navbar = document.querySelector(".navbar__container");
 const mouseBody = document.querySelector("#mouse_body");
 window.addEventListener("scroll", function (e) {
   e.preventDefault();
-  if (window.scrollY >= 700) {
+  if (window.scrollY >= 400) {
     navbar.classList.add("acti");
     // console.log("jj");
   } else {
@@ -15,13 +15,17 @@ const navBar = document.querySelector(".navbar__container__bars");
 const navMenu = document.querySelector(".navbar__container__items");
 
 navBar.addEventListener("click", () => {
-    navBar.classList.toggle("active");
-  navMenu.classList.toggle("active");
+    navBar.classList.toggle("barsActive");
+  navMenu.classList.toggle("navActive");
 });
 
 document.querySelectorAll(".nav-link").forEach((link) =>
   link.addEventListener("click", () => {
-    navBar.classList.remove("active");
-    navMenu.classList.remove("active");
+    navBar.classList.remove("barsActive");
+    navMenu.classList.remove("navActive");
   })
 );
+const burgerBtn = document.querySelector(".burger");
+const nav = document.querySelector(".nav-links");
+const navLinks = document.querySelectorAll(".nav-links li");
+
